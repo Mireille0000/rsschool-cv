1efe19e2de21a73b4c88a7a5af9fb191ec5a4ab5
Gasanova Tamara <solas93@mail.ru>
2023-07-07 16:57:54 +0300

    feat: add custom button, js

---
+++ main.js
@@ -0,0 +1,9 @@
+(function () {
+    const buttonProject = document.querySelector('.button_project');
+    const project = document.querySelector('.project');
+
+    buttonProject.addEventListener('click', ()=>{
+        project.classList.toggle('project_active')
+    } )
+} ()
+)
